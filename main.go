a9a4c3a13e2403986dba69439f3a90c29a19b8c0
face4 <progface4@gmail.com>
2023-09-10 18:23:45 +0900

    55

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+	}
+
 	err := http.ListenAndServe(
 		":18080",
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -18,4 +26,34 @@ func main() {
 		fmt.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+
+	// 別goroutineのエラーを受け取るためにerrgroupを利用
+	eg, ctx := errgroup.WithContext(ctx)
+	// 別goroutineでサーバーを起動
+	eg.Go(
+		func() error {
+			// ErrServerClosedは正常なので除外
+			if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				log.Printf("failed to close %+v", err)
+				return err
+			}
+			return nil
+		},
+	)
+
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	return eg.Wait()
 }
