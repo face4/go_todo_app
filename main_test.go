8f6e1c7b56d9c9dcc2d7af82b589953db909162e
face4 <progface4@gmail.com>
2023-09-10 19:21:49 +0900

    56-fix

--- main_test.go
+++ main_test.go
@@ -13,6 +13,9 @@ import (
 
 func TestRun(t *testing.T) {
 	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to listen port %v", err)
+	}
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
